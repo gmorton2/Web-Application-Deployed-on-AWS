86cc80a5376bbc9f6f6503791e6f7fdf2b689fcd
Garinn Morton <gmorton2@csu.fullerton.edu>
2020-10-16 00:39:00 -0700

    fixed bug

--- app.js
+++ app.js
@@ -3,20 +3,23 @@
 const fd = require('fs');
 const http = require('http');
 
-
+const server = http.createServer((req, res) => {
+	console.log(req.url, req.method);
 	//set header content type
 	res.setHeader('Content-Type', 'text/html');
 	//send an html file
-	fs.readFile('./UI/index.html', (err, DataCue)) => {
-		if (err) {
-			console.log(err);
-			res.end();
-		}
-		else {
-			//res.write(data);
-			res.end();
-        }
-    })
+	fs.readFile('./UI/index.html', (err, DataCue) => {
+	if (err) {
+		console.log(err);
+		res.end();
+	}
+	else {
+		//res.write(data);
+		res.end();
+	}
+})
+	
+    
 });
 
 const port = process.env.port || 3000
