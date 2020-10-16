aa983e884a31553b8a2e31a2978632f49832a65e
Garinn Morton <gmorton2@csu.fullerton.edu>
2020-10-15 23:58:19 -0700

    Change to app.js to read html file

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const app = express();
+const fd = require('fs');
 
 app.get('/', (req, res) => {
-	res.send("Welcome");
+	//res.send("Welcome");
+	//set header content type
+	res.setHeader('Content-Type', 'text/html');
+	//send an html file
+	fs.readFile('./UI/index.html', (err, DataCue)) => {
+		if (err) {
+			console.log(err);
+			res.end();
+		}
+		else {
+			res.write(data);
+			res.end();
+        }
+    })
 });
 
 const port = process.env.port || 3000
